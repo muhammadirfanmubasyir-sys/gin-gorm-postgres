77d2dccb9458bf2cd2cf2cfeb8416c9b0d4fc71c
Muhammad Irfan Mubasyir <muhammadirfanmubasyir@gmail.com>
2026-04-20 12:19:53 +0700

    bug fixing

--- models/user.go
+++ models/user.go
@@ -1,9 +1,6 @@
 package models
-
-import "gorm.io/gorm"
-
+ 
 type User struct {
-	gorm.Model
 	Id       int    `json:"id" gorm:"primary_key"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
