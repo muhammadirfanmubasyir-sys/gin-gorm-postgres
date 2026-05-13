4ac61e0f643f5007b0d2c3acc70040bb06786086
Muhammad Irfan Mubasyir <muhammadirfanmubasyir@gmail.com>
2026-05-13 15:25:49 +0700

    bug fix

--- models/user.go
+++ models/user.go
@@ -1,8 +1,12 @@
 package models
- 
+
 type User struct {
 	Id       int    `json:"id" gorm:"primary_key"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func (User) TableName() string {
+	return "table_of_user"
 }
